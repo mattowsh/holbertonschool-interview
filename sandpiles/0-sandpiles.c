a6d7069de695ec8326d47eaca943329f0ee0661b
mattowsh <mattobelen@gmail.com>
2023-04-21 14:02:55 -0300

    betty style ok

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -4,27 +4,25 @@
 /**
  * grid_status - Check the status of a grid
  * @grid1: a 3x3 grid
- * 
  * Return: 1 if a grid is stable, 0 otherwise
 */
 
 int grid_status(int grid1[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid1[i][j] > 3)
-                return 0; 
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+		if (grid1[i][j] > 3)
+			return (0);
+		}
+	}
 
-    /* At this point, the grid is stable :) */
-    return 1;
+	/* At this point, the grid is stable :) */
+	return (1);
 }
-
 
 /**
  * add_grids - Excecute the math operation: sum
@@ -34,15 +32,15 @@ int grid_status(int grid1[3][3])
 
 void add_grids(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
 }
 
 /**
@@ -52,60 +50,57 @@ void add_grids(int grid1[3][3], int grid2[3][3])
 
 void print_grid(int grid1[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            printf("%d", grid1[i][j]);
-            if (j != 2)
-                printf(" ");
-        }
-        printf("\n");
-    }
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			printf("%d", grid1[i][j]);
+			if (j != 2)
+				printf(" ");
+		}
+		printf("\n");
+	}
 }
 
 /**
  * sandpiles_sum - Computes the sum of two sandpiles
  * @grid1: Left 3x3 grid
  * @grid2: Right 3x3 grid
- * 
  * Return: a 3x3 grid with the sum result
  */
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int new_grid[3][3];
+	int new_grid[3][3];
 
-    /* Add the two grids */
-    add_grids(grid1, grid2);
+	add_grids(grid1, grid2);
 
-    /* Check status of the resulting grid: if isn't stable --> toppling */
-    while (!grid_status(grid1))
-    {
-        int i, j;
-        
-        /* Print the current grid result that is saved in grid1 */
-        printf("=\n");
-        print_grid(grid1);
+	/* Check status of the resulting grid: if isn't stable --> toppling */
+	while (!grid_status(grid1))
+	{
+		int i, j;
+		/* Print the current grid result that is saved in grid1 */
+		print_grid(grid1);
 
-        /* Initialize the new grid */
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-                new_grid[i][j] = 0;
-        }
+		/* Initialize the new grid */
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+				new_grid[i][j] = 0;
+		}
 
-        /* Performe toppling if any value is > 3 */
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                if (grid1[i][j] > 3)
-                {
-                    grid1[i][j] -= 4;
-                    if ((i - 1 >= 0) && (i - 1 < 3))
+		/* Performe toppling if any value is > 3 */
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (grid1[i][j] > 3)
+				{
+					grid1[i][j] -= 4;
+					if ((i - 1 >= 0) && (i - 1 < 3))
 						new_grid[i - 1][j] += 1;
 					if ((j - 1 >= 0) && (j - 1 < 3))
 						new_grid[i][j - 1] += 1;
@@ -113,11 +108,10 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 						new_grid[i + 1][j] += 1;
 					if ((j + 1 >= 0) && (j + 1 < 3))
 						new_grid[i][j + 1] += 1;
-                }
-            }
-        }
-
-        /* Performe the operation with the new grid */
-        add_grids(grid1, new_grid);
-    }
+				}
+			}
+		}
+		/* Performe the operation with the new grid */
+		add_grids(grid1, new_grid);
+	}
 }
